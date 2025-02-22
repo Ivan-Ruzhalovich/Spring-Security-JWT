e58284558d6d5d4bd2112fba2f85065676bb31a5
Ivan-Ruzhalovich <ruzhalovich@bk.ru>
2025-02-22 14:44:43 +0300

    Delete src/main/java/com/example/springsecurityjwt/service directory

--- src/main/java/com/example/springsecurityjwt/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.example.springsecurityjwt.services;
-
-import com.example.springsecurityjwt.entity.OurUsers;
-import com.example.springsecurityjwt.models.RequestModel;
-
-public interface AdminService {
-    OurUsers unlock(RequestModel requestModel);
-    OurUsers lock(RequestModel requestModel);
-}
--- src/main/java/com/example/springsecurityjwt/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.example.springsecurityjwt.services;
-
-import com.example.springsecurityjwt.models.RequestModel;
-
-public interface AuthService {
-    RequestModel registration(RequestModel requestModel);
-    RequestModel singIn(RequestModel requestModel);
-}
